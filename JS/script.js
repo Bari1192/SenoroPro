454e670a12145ea5075a0016631486a11d2c5468
szekeres3 <abelsz100@gmail.com>
2023-11-24 18:57:54 +0100

    sötét-világos, laptopok feltöltése, leírás, képek, újdizájn

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,71 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+let block = false;
+let keyToWrite = false;
+let x = document.getElementById("doboz");
+function print(text) {
+    (function checkFlag() {
+        if(block && !keyToWrite){
+            window.setTimeout(checkFlag, 150);
+        }
+        else{
+            block = true;
+            let i = 0;
+            (function type(){
+                x.innerHTML += (text.charAt(i));
+                if (++i < text.length){
+                    setTimeout(type, 50);
+                }
+                else{
+                    x.innerHTML += "<br>";
+                    block=false;
+                    setTimeout(() => {
+                        document.getElementById('zene').click();
+                        x.style.display = "none";
+                    }, "1500");  
+                }
+            })();
+        }
+    })();
+}
+function temavaltas(x){
+    let elemek = document.getElementsByClassName("atvezeto");
+    if (elemek[0].src.includes('light')){
+        document.getElementById('navos').classList.add('navbar-dark', 'bg-dark');
+        for (let i = 0; i < elemek.length; i++) {
+            elemek[i].src = "../Kepek/laptop-kepek/thinkpad-dark-theme-"+(i+1)+".jpeg";
+            elemek[i].alt = "sötét laptop "+(i+1);
+        }
+        elemek = document.getElementsByClassName('container');
+        for (let i = 0; i < elemek.length; i++){
+            elemek[i].classList.add('bg-dark', 'text-white');
+            elemek[i].classList.remove('bg-white', 'text-dark');
+        }
+        x.classList.add('btn-light');
+        x.classList.remove('btn-dark');
+    }
+    else {
+        if (elemek[0].src.includes('dark')){
+            document.getElementById('navos').classList.remove('navbar-dark', 'bg-dark');
+            for (let i = 0; i < elemek.length; i++) {
+                elemek[i].src = "../Kepek/laptop-kepek/thinkpad-light-theme-"+(i+1)+".jpeg";
+                elemek[i].alt = "vilagos laptop "+(i+1);
+            }
+            elemek = document.getElementsByClassName('container');
+            for (let i = 0; i < elemek.length; i++){
+                elemek[i].classList.remove('bg-dark', 'text-white');
+                elemek[i].classList.add('bg-white', 'text-dark');
+            }
+            x.classList.remove('btn-light');
+            x.classList.add('btn-dark');
+        }
+    }
+}
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.altKey && event.key === 'g') {
+        x.style.display = "flex";
+        x.innerHTML = ""; /*általános felhasználhatóság miatt itt törlök*/
+        print('Hey Sziri! Aleksza játssz le rap zenét!');
+        console.log(window.navigator.userAgent.toString())
+    }
+});
