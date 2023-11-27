4e492e8299418c78c136f778d445db205ec614c2
szekeres3 <abelsz100@gmail.com>
2023-11-27 01:12:37 +0100

    'foreach es igazitas uj navhoz'

--- JS/script.js
+++ JS/script.js
@@ -1,67 +1,82 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 let block = false;
-let keyToWrite = false;
 let x = document.getElementById("doboz");
 function print(text) {
     (function checkFlag() {
-        if(block && !keyToWrite){
+        if (block) {
             window.setTimeout(checkFlag, 150);
         }
-        else{
+        else {
             block = true;
             let i = 0;
-            (function type(){
+            (function type() {
                 x.innerHTML += (text.charAt(i));
-                if (++i < text.length){
+                if (++i < text.length) {
                     setTimeout(type, 50);
                 }
-                else{
+                else {
                     x.innerHTML += "<br>";
-                    block=false;
+                    block = false;
                     setTimeout(() => {
                         document.getElementById('zene').click();
                         x.style.display = "none";
-                    }, "1500");  
+                    }, "1500");
                 }
             })();
         }
     })();
 }
-function temavaltas(x){
+function temavaltas(x) {
     let elemek = document.getElementsByClassName("atvezeto");
-    if (elemek[0].src.includes('light')){
+    if (elemek[0].src.includes('light')) {
+        document.getElementsByClassName('dropdown-menu')[0].classList.add('dropdown-menu-dark');
         document.getElementById('navos').classList.add('navbar-dark', 'bg-dark');
         for (let i = 0; i < elemek.length; i++) {
-            elemek[i].src = "../Kepek/laptop-kepek/thinkpad-dark-theme-"+(i+1)+".jpeg";
-            elemek[i].alt = "sötét laptop "+(i+1);
+            elemek[i].src = "../Kepek/laptop-kepek/thinkpad-dark-theme-" + (i + 1) + ".jpeg";
+            elemek[i].alt = "sötét laptop " + (i + 1);
         }
-        elemek = document.getElementsByClassName('container');
-        for (let i = 0; i < elemek.length; i++){
-            elemek[i].classList.add('bg-dark', 'text-white');
-            elemek[i].classList.remove('bg-white', 'text-dark');
+        ['container', 'offcanvas', 'card', 'container-fluid'].forEach(element => {
+            elemek = document.getElementsByClassName(element);
+            for (let i = 0; i < elemek.length; i++) {
+                elemek[i].classList.add('bg-dark', 'text-white');
+                elemek[i].classList.remove('bg-white', 'text-dark');
+            }
+        });
+        elemek = document.getElementsByClassName('menuk');
+        for (let i = 0; i < elemek.length; i++) {
+            elemek[i].classList.add('btn-dark');
+            elemek[i].classList.remove('btn-light');
         }
         x.classList.add('btn-light');
         x.classList.remove('btn-dark');
     }
     else {
-        if (elemek[0].src.includes('dark')){
+        if (elemek[0].src.includes('dark')) {
             document.getElementById('navos').classList.remove('navbar-dark', 'bg-dark');
+            document.getElementsByClassName('dropdown-menu')[0].classList.remove('dropdown-menu-dark');
             for (let i = 0; i < elemek.length; i++) {
-                elemek[i].src = "../Kepek/laptop-kepek/thinkpad-light-theme-"+(i+1)+".jpeg";
-                elemek[i].alt = "vilagos laptop "+(i+1);
+                elemek[i].src = "../Kepek/laptop-kepek/thinkpad-light-theme-" + (i + 1) + ".jpeg";
+                elemek[i].alt = "vilagos laptop " + (i + 1);
             }
-            elemek = document.getElementsByClassName('container');
-            for (let i = 0; i < elemek.length; i++){
-                elemek[i].classList.remove('bg-dark', 'text-white');
-                elemek[i].classList.add('bg-white', 'text-dark');
+            elemek = document.getElementsByClassName('menuk');
+            for (let i = 0; i < elemek.length; i++) {
+                elemek[i].classList.remove('btn-dark');
+                elemek[i].classList.add('btn-light');
             }
+            ['container', 'offcanvas', 'card', 'container-fluid'].forEach(element => {
+                elemek = document.getElementsByClassName(element);
+                for (let i = 0; i < elemek.length; i++) {
+                    elemek[i].classList.remove('bg-dark', 'text-white');
+                    elemek[i].classList.add('bg-white', 'text-dark');
+                }
+            });
             x.classList.remove('btn-light');
             x.classList.add('btn-dark');
         }
     }
 }
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.ctrlKey && event.altKey && event.key === 'g') {
         x.style.display = "flex";
         x.innerHTML = ""; /*általános felhasználhatóság miatt itt törlök*/
